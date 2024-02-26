049e625ce0fedc77750ace256ca22f799004fd24
ormesino <pedromateus18@hotmail.com>
2024-02-26 17:50:29 -0300

    config: add cookie from fastify

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,15 +1,20 @@
-import fastify from 'fastify';
-import { createPoll, deletePoll, getPoll } from './controllers/poll.ts';
+import cookie from "@fastify/cookie";
+import fastify from "fastify";
+import { createPoll, deletePoll, getPoll } from "./controllers/poll.ts";
 const app = fastify();
+
+app.register(cookie, {
+  secret: "my-secret",
+});
 
 app.register(createPoll);
 app.register(getPoll);
 app.register(deletePoll);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('🌶️ Server listening at http://localhost:3333');
+  console.log("🌶️ Server listening at http://localhost:3333");
 });
 
-app.get('/', () => {
-  return 'Hello, world!';
+app.get("/", () => {
+  return "Hello, world!";
 });
